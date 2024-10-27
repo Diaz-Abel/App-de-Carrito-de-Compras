5c9592075c189870d0e5ccdccc0550f109137261
AlexArce2000 <alexisroarce2000@gmail.com>
2024-10-27 18:21:33 -0300

    fix editar productos

--- app/src/main/java/com/fpuna/carrito/views/EditarProductoView.kt
+++ app/src/main/java/com/fpuna/carrito/views/EditarProductoView.kt
@@ -17,6 +17,7 @@ import com.fpuna.carrito.models.Producto
 import com.fpuna.carrito.viewmodel.CategoriaViewModel
 import com.fpuna.carrito.viewmodel.ProductoViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditarProductoView(
     paddingValues: PaddingValues,
@@ -38,93 +39,115 @@ fun EditarProductoView(
         selectedCategoria = listaCategorias.find { it.id == producto.idCategoria }
     }
 
-    Column(
-        modifier = Modifier
-            .padding(paddingValues)
-            .padding(top = 30.dp)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(
-            value = nombre,
-            onValueChange = { nombre = it },
-            label = { Text(text = "Nombre del Producto") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 30.dp)
-                .padding(bottom = 15.dp)
-        )
-
-        OutlinedTextField(
-            value = precioVenta,
-            onValueChange = { precioVenta = it },
-            label = { Text(text = "Precio de Venta") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 30.dp)
-                .padding(bottom = 15.dp)
-        )
-
-        // Botón para seleccionar categoría
-        Button(
-            onClick = { showDialog = true },
-            modifier = Modifier
-                .padding(horizontal = 30.dp)
-                .fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.secondary,
-                contentColor = Color.White
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text("Editar Producto", color = Color.White)
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Regresar",
+                            tint = Color.White
+                        )
+                    }
+                }
             )
-        ) {
-            Text(text = if (selectedCategoria != null) "Categoría: ${selectedCategoria!!.name}" else "Selecciona una categoría")
         }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(top = 30.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = nombre,
+                onValueChange = { nombre = it },
+                label = { Text(text = "Nombre del Producto") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 30.dp)
+                    .padding(bottom = 15.dp)
+            )
 
-        if (showDialog) {
-            AlertDialog(
-                onDismissRequest = { showDialog = false },
-                title = { Text("Seleccionar Categoría") },
-                text = {
-                    Column {
-                        listaCategorias.forEach { categoria ->
-                            Text(
-                                text = categoria.name,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable {
-                                        selectedCategoria = categoria // Actualiza la categoría seleccionada
-                                        showDialog = false // Cierra el diálogo
-                                    }
-                                    .padding(8.dp)
-                            )
+            OutlinedTextField(
+                value = precioVenta,
+                onValueChange = { precioVenta = it },
+                label = { Text(text = "Precio de Venta") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 30.dp)
+                    .padding(bottom = 15.dp)
+            )
+
+            // Botón para seleccionar categoría
+            Button(
+                onClick = { showDialog = true },
+                modifier = Modifier
+                    .padding(horizontal = 30.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = if (selectedCategoria != null) "Categoría: ${selectedCategoria!!.name}" else "Selecciona una categoría")
+            }
+
+            if (showDialog) {
+                AlertDialog(
+                    onDismissRequest = { showDialog = false },
+                    title = { Text("Seleccionar Categoría") },
+                    text = {
+                        Column {
+                            listaCategorias.forEach { categoria ->
+                                Text(
+                                    text = categoria.name,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .clickable {
+                                            selectedCategoria = categoria // Actualiza la categoría seleccionada
+                                            showDialog = false // Cierra el diálogo
+                                        }
+                                        .padding(8.dp)
+                                )
+                            }
+                        }
+                    },
+                    confirmButton = {
+                        TextButton(onClick = { showDialog = false }) {
+                            Text("Cerrar")
                         }
                     }
-                },
-                confirmButton = {
-                    TextButton(onClick = { showDialog = false }) {
-                        Text("Cerrar")
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    if (nombre.isNotEmpty() && precioVenta.isNotEmpty() && selectedCategoria != null) {
+                        val productoActualizado = Producto(
+                            idProducto = producto.idProducto,
+                            nombre = nombre,
+                            precioVenta = precioVenta.toDoubleOrNull() ?: 0.0,
+                            idCategoria = selectedCategoria!!.id
+                        )
+                        productoViewModel.actualizarProducto(productoActualizado)
+                        navController.popBackStack()
                     }
-                }
-            )
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(
-            onClick = {
-                if (nombre.isNotEmpty() && precioVenta.isNotEmpty() && selectedCategoria != null) {
-                    val productoActualizado = Producto(
-                        idProducto = producto.idProducto,
-                        nombre = nombre,
-                        precioVenta = precioVenta.toDoubleOrNull() ?: 0.0,
-                        idCategoria = selectedCategoria!!.id
-                    )
-                    productoViewModel.actualizarProducto(productoActualizado)
-                    navController.popBackStack()
-                }
-            },
-            modifier = Modifier.padding(horizontal = 30.dp)
-        ) {
-            Text(text = "Guardar Cambios")
+                },
+                modifier = Modifier.padding(horizontal = 30.dp)
+            ) {
+                Text(text = "Guardar Cambios")
+            }
         }
     }
 }
